44919126fdacaccd278d585b9bcbadf5e2e01ca5
albertofabrizio <alberto.fabrizio@gmail.com>
2015-07-17 10:42:37 +0200

    Adesso read_out dovrebbe andare bene

--- src/master/computation.py
+++ src/master/computation.py
@@ -68,13 +68,14 @@ class GenericRun(object):
 
     def __init__(self):
         self.dormi = 60
+        self.timeout=0
         self.timeoutMAX = 10
         self.command = 'uname -a'
 
 
     def write_input(self, xyzp, cpath):
         self.name = xyzp[:-3]
-        Input(filep).write(self.name + 'inp')
+        Input(xyzp).write(self.name + 'inp')
 
 
     def runall(self):
@@ -93,16 +94,19 @@ class RunGamess(GenericRun):
 
 
     def read_out(self):
-        for self.timeout in enumerate(self.timeoutMAX):
+        while True:
             time.sleep(self.dormi)
             with open(self.name + ".log", mode='r') as g:
                 for line in g:
-                    if "exited gracefully" in line or self.timeout > self.timeoutMAX:
+                    if "exited gracefully" in line
                         break
                     else:
-                        print('Something is missing here?')
-                        pass
-            break
+                        print('Exited gracefully not found.')
+                        
+            for self.timeout in enumerate(self.timeoutMAX):
+                if int(self.timeout) > int(self.timeoutMAX):
+                    print('File not found.')
+                    
             self.timeout += 1
 
     def move_density(self, dest):
