f615ec75f54400d188bc433f348533917289aee4
Riccardo Petraglia <riccardo.petraglia@epfl.ch>
2015-07-17 09:47:05 +0200

    Something is missing in computation?

--- src/master/computation.py
+++ src/master/computation.py
@@ -49,7 +49,7 @@ except subprocess.CalledProcessError:
 
 __author__ = 'Riccardo Petraglia'
 __credits__ = ['Riccardo Petraglia']
-__updated__ = "2015-07-16"
+__updated__ = "2015-07-17"
 __license__ = 'GPLv2'
 __version__ = git_v
 __maintainer__ = 'Riccardo Petraglia'
@@ -67,20 +67,20 @@ from make_input import Input
 class GenericRun(object):
 
     def __init__(self):
-        self.dormi=60
-        self.timeoutMAX=10
+        self.dormi = 60
+        self.timeoutMAX = 10
         self.command = 'uname -a'
-       
-        
-    def write_input(self,xyzp,cpath):
-        self.name=xyzp[:-3]
-        Input(filep).write(self.name+'inp')
-        
+
+
+    def write_input(self, xyzp, cpath):
+        self.name = xyzp[:-3]
+        Input(filep).write(self.name + 'inp')
+
 
     def runall(self):
         print('Should run {}'.format(self.command))
-        out=open(self.name+"log", mode='w')
-        subprocess.call(self.command,stdout=out)
+        out = open(self.name + "log", mode='w')
+        subprocess.call(self.command, stdout=out)
 
 
 
@@ -88,28 +88,31 @@ class GenericRun(object):
 class RunGamess(GenericRun):
 
     def __init__(self):
-        self.command = ["/software/gamess/rungms",self.name+"inp", "13" , "1"] #DA Vedere!!!!!!!!!
+        self.command = ["/software/gamess/rungms", self.name + "inp", "13" , "1"]  #DA Vedere!!!!!!!!!
 #        self.command = ["/home/student1/GAMESS/rungms",self.name+"inp","00","1"]
-    
-    
+
+
     def read_out(self):
         for self.timeout in enumerate(self.timeoutMAX):
             time.sleep(self.dormi)
-            with open(self.name+".log", mode='r') as g:
+            with open(self.name + ".log", mode='r') as g:
                 for line in g:
                     if "exited gracefully" in line or self.timeout > self.timeoutMAX:
                         break
+                    else:
+                        print('Something is missing here?')
+                        pass
             break
             self.timeout += 1
 
-    def move_density(self,dest):
+    def move_density(self, dest):
         shutil.copy(PARAM_UNF.dat, dest)
-        
+
 
 class RunDensity(GenericRun):
 
     def __init__(self):
-        self.command = ["./START.X"] #DA Vedere!!!!!!!!!
+        self.command = ["./START.X"]  #DA Vedere!!!!!!!!!
 
 
 class RunDdsc(GenericRun):
