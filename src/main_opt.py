e0b81b188a1930bde4c6c686d44d3a0188d7b414
Alberto Fabrizio <afabrizi@lcmd30.cluster>
2015-09-28 14:03:10 +0200

    FULL gamess loop

--- src/main_opt.py
+++ src/main_opt.py
@@ -4,6 +4,7 @@
 import sys, os
 import logging as lg
 from scipy.optimize import minimize
+import shutil
 
 # Easier to port
 home = os.path.expanduser("~")
@@ -25,25 +26,14 @@ def main():
     init_logging()
     # init computation
     prms = Parameters()
-    i=0
-    while i !=2:
+
+    for i in range(0,2):
        index='DENS-'+str(i)
+       print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ", i)
        Run(run_name=config['Name'], tset_path=config['TraininSetPath']).index = index
        trset = TrainingSet(config['TraininSetPath'], config['TraininSetName'])
-       i+=1
-    # MolSet.p_call_mol_energy('full')
-    # print('NEEDEDMOL:',list(map(lambda x: x.id,trset.container[0].container[0].needed_mol)))
-    # print('UNIENERGY:',list(map(lambda x: x.uni_energy,trset.container[0].container[0].needed_mol)))
-    # print('FULLENERGY:',list(map(lambda x: x.full_energy,trset.container[0].container[0].needed_mol)))
-    # print('FUNCENERGY:',list(map(lambda x: x.func_energy,trset.container[0].container[0].needed_mol)))
-#    sys.exit()
-    # MolSet.p_call_mol_energy('func')
-    # for mol in MolSet.container:
-    #     print(mol.myprm_full.sprms)
-    #     print(mol.myprm_func.sprms)
 
-
-    prms.prms = dict(tta=[13.300000190734863],
+       prms.prms = dict(tta=[13.300000190734863],
                        ttb=[1.5299999713897705],
                        cxhf=[0.157706],
                        omega=[0.3],
@@ -51,31 +41,32 @@ def main():
                        cc_aa=[1.000000, -4.33879, 18.2308, -31.7430, 17.2901],
                        cc_ab=[1.000000, 2.37031, -11.3995, 6.58405, -3.78132])
 
-    print(prms._parameters)
+       print(prms._parameters)
 #    prms.prms = {'cx_aa_0': 0.5, 'cc_aa_2':12000, 'tta':100}
 #    print(trset.compute_MAE('full'))
 #    print(prms._parameters)
 
-    prms.optim = ['tta', 'cx_aa_0','cx_aa_1','cx_aa_2','cx_aa_3']
-    print(prms.optim)
+       prms.optim = ['tta', 'cx_aa_0','cx_aa_1','cx_aa_2','cx_aa_3']
+       print(prms.optim)
     
-    x0_=[13.3, 0.842294, 0.726479, 1.0476, -5.70635]
+       x0_=[13.3, 0.842294, 0.726479, 1.0476, -5.70635]
 #    x1_=[13.3, 0.04334172, 2.14371968, 2.0473701, -5.10893517,  13.63496978]
 #    x2_=[13.4, 0.05334172, 2.14371968, 2.0473701, -5.10893517,  13.63496978]
 
-    def printer(xc):
-       print('END of STEP')
-       print(xc)
+       def printer(xc):
+          print('END of STEP')
+          print(xc)
 
-    bnds=((None,None),(0,None),(None,None),(None,None),(None,None))
-    print(trset.optimizer(x0_,'full','MAE'))
+       bnds=((None,None),(0,None),(None,None),(None,None),(None,None))
+       print(trset.optimizer(x0_,'full','MAE'))
 #   print(trset.optimizer(x1_,'func','MAE'))
 #   print(trset.optimizer(x2_,'func','MAE'))
 #   print(minimize(trset.optimizer,x0_,args=('func','MAE'),method='L-BFGS-B', callback=printer, bounds=bnds,  options={'disp': True} ))
 
-    print(minimize(trset.optimizer,x0_,args=('func','MAE'), method='TNC', bounds=bnds, callback=printer, options={'disp': True,'gtol': 1e-11 }))
+       print(minimize(trset.optimizer,x0_,args=('func','MAE'), method='TNC', bounds=bnds, callback=printer, options={'disp': True,'gtol': 1e-11, 'maxiter':2 }))
 #    print(minimize(trset.optimizer,x0_,args=('func','MAE'), method='L-BFGS-B', bounds=bnds, callback=printer, options={'disp': True,'gtol': 1e-2 }))
-
+       shutil.copy('/scratch/TMP_DATA/FUNC_PAR.dat', os.path.join(home, 'wb97xddsc/TMP_DATA'))
+       shutil.copy('/scratch/TMP_DATA/a0b0', os.path.join(home, 'wb97xddsc/TMP_DATA'))
 
 def init_logging():
     if os.path.isfile(config['logfile']):
